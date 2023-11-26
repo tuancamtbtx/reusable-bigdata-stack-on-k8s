3eacf8a2a659a021496dce8fd5cc45a953b937c8
Tuan.Nguyen3 <tuan.nguyen3@trustingsocial.com>
2023-11-26 22:11:31 +0700

    [add] some operator init

---
+++ operators/ts/k8s-operator/operators/JavaApplicationOperator.ts
@@ -0,0 +1,23 @@
+import Operator, { ResourceEventType, ResourceEvent } from '@dot-i/k8s-operator';
+
+class JavaApplicationOperator extends Operator {
+    protected async init() {
+        await this.watchResource('', 'v1', 'namespaces', async (e) => {
+            const object = e.object;
+            const metadata = object.metadata;
+            switch (e.type) {
+                case ResourceEventType.Added:
+                    // do something useful here
+                    break;
+                case ResourceEventType.Modified:
+                    // do something useful here
+                    break;
+                case ResourceEventType.Deleted:
+                    // do something useful here
+                    break;
+            }
+        });
+    }
+
+}
+export default JavaApplicationOperator;
